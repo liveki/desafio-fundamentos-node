1ec6037dc88f4fc0efd8cdf05d2863ee348be8e0
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-04-15 02:24:09 -0300

    implementado os métodos do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,65 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance.income = this.getTotalIncomes();
+    this.balance.outcome = this.getTotalOutcomes();
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  private getTotalIncomes(): number {
+    const sum = this.transactions.reduce((total, next) => {
+      if (next.type === 'income') return total + next.value;
+      return total;
+    }, 0);
+
+    return sum;
+  }
+
+  private getTotalOutcomes(): number {
+    const sum = this.transactions.reduce((total, next) => {
+      if (next.type === 'outcome') return total + next.value;
+      return total;
+    }, 0);
+
+    return sum;
+  }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
